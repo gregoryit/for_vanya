d42284635c77ed9f8102400135748396dd74a7c5
Gregory Baranov <47480142+GregoryBaranov@users.noreply.github.com>
2021-03-21 13:03:34 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,83 +1,56 @@
-from turtle import *
-a = input('Введите размер пера:')
-t = Turtle()
-t.color('black')
-t.width(a)
-t.shape('circle')
-t.pendown()
-t.speed(3999)
-print('1 - зелёный , 2 - красный , 3 - желтый ,4 - синий , 5 - розовый , 6 - коричневый , 7 - чёрный         > вправо , < влево , ↑ вверх  , ↓ вниз , 0 - круг , w - размер 8 , s - размер 4 , e - начать заливку , q - закончить заливку ')
-
-def draw(x ,y):
-    t.goto(x, y)
-def move(x, y):
-    t.penup()
-    t.goto(x , y)
-    t.pendown()
-
-def setGreen():
-    t.color('green')
-
-def setRed():
-    t.color('red')
-
-def setYellow():
-    t.color('Yellow')
-
-
-def setBlue():
-    t.color('blue')
-
-def setPink():
-    t.color('#ff008c')
-
-def setBrown():
-    t.color('#964B00')
-
-def setBlack():
-    t.color('black')
-
-def stepRight():
-    t.goto(t.xcor() + 5, t.ycor())
-def stepLeft():
-    t.goto(t.xcor() - 5, t.ycor())
-def stepUp():
-    t.goto(t.xcor() , t.ycor() + 5)
-def stepDown():
-    t.goto(t.xcor() , t.ycor() - 5)
-def stepCircle():
-    t.circle(30)
-def setW():
-    t.width(8)
-    
-def setS():
-    t.width(4)
-
-def setBegin_fill():
-    t.begin_fill()
-def setEnd_fill():
-    t.end_fill()
-
-
-scr = t.getscreen()
-scr.listen()
-scr.onscreenclick(move)
-scr.onkey(setBlack, '7')
-scr.onkey(stepRight, 'Right')
-scr.onkey(stepLeft, 'Left')
-scr.onkey(setEnd_fill, 'q')
-scr.onkey(setBegin_fill, 'e')
-scr.onkey(setS, 's')
-scr.onkey(stepCircle, '0')
-scr.onkey(stepDown, 'Down')
-scr.onkey(stepUp, 'Up')
-scr.onkey(setW, 'w')
-scr.onkey(setBrown, '6')
-scr.onkey(setPink, '5')
-scr.onkey(setYellow, '3')
-scr.onkey(setRed, '2')
-scr.onkey(setGreen, '1')
-scr.onkey(setBlue, '4')
-t.ondrag(draw)
-
-
+from time import sleep
+ 
+class Hero():
+   #конструктор класса
+   def __init__(self, name, health, armor):
+       self.name = name
+       self.health = health #число
+       self.armor = armor #строка
+   #печать параметров персонажа:
+   def print_info(self):
+       print('Уровень здоровья:', self.health)
+       print('Класс брони:', self.armor, '\n')
+ 
+'''То, что выше, дано в уровне изначально'''
+ 
+class Warrior(Hero):
+   def hello(self):
+       print('-> НОВЫЙ ГЕРОЙ. Верхом на коне появился бравый воин по имени', self.name)
+       self.print_info()
+       sleep(4)
+ 
+   def attack(self, enemy):
+       print('-> УДАР! Храбрый воин', self.name, 'атакует', enemy.name, 'мечом!')
+       enemy.armor -= 15 #сила удара для класса Воин
+       if enemy.armor < 0:
+           enemy.health += enemy.armor
+           enemy.armor = 0
+       print('Страшный удар обрушился на противника. \nТеперь его броня: ' +
+           str(enemy.armor) + ', а уровень здоровья: ' + str(enemy.health) + '\n')
+       sleep(5)
+ 
+class Magician(Hero):
+   def hello(self):
+       print('-> НОВЫЙ ГЕРОЙ. Откуда ни возьмись появился искусный волшебник', self.name)
+       self.print_info()
+       sleep(4)
+ 
+   def attack(self, enemy):
+       print('-> УДАР! Ловкий маг', self.name, 'накладывает заклинание на', enemy.name)
+       enemy.armor -= 35 #сила удара для класса Маг
+       if enemy.armor < 0:
+           enemy.health += enemy.armor
+           enemy.armor = 0
+       print('Сложное заклинание оглушило противника. \nТеперь его броня: ' +
+           str(enemy.armor) + ', а уровень здоровья: ' + str(enemy.health) + '\n')
+       sleep(5)
+ 
+ 
+Warrior1 = Warrior('Henry', 100, 50)
+Warrior1.hello()
+ 
+Magician1 = Magician('Luke', 50, 20)
+Magician1.hello()
+ 
+Warrior1.attack(Magician1)
+Magician1.attack(Warrior1)
